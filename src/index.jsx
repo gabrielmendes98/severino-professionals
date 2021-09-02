5a437978d88284475e133de69712138b5c39d839
gabriel santiago <gabrielmssantiago@gmail.com>
2021-09-02 00:20:40 -0300

    remove console.log

--- src/index.jsx
+++ src/index.jsx
@@ -3,8 +3,6 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-console.log(process.env.NODE_ENV);
-
 if (process.env.NODE_ENV === 'development' && process.env.REACT_APP_MOCK_ON) {
   const { worker } = require('./services/mocks/browser');
   worker.start({
